af8031047af1af7b0c55592f07994e221187673c
unknown <ladrera21@gmail.com>
2026-04-30 17:45:14 +0800

    updating database

--- database/saint_vincent_farm.sql
+++ database/saint_vincent_farm.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 22, 2026 at 05:59 PM
--- Server version: 10.4.32-MariaDB
+-- Generation Time: Apr 30, 2026 at 11:44 AM
+-- Server version: 8.0.34
 -- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -28,12 +28,13 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `cart` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `product_id` int(11) NOT NULL,
-  `quantity` int(11) NOT NULL DEFAULT 1,
-  `amount` int(11) NOT NULL DEFAULT 1,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `product_id` int NOT NULL,
+  `quantity` int NOT NULL DEFAULT '1',
+  `amount` int NOT NULL DEFAULT '1',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `cart` tinyint NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -43,8 +44,8 @@ CREATE TABLE `cart` (
 --
 
 CREATE TABLE `category` (
-  `id` int(11) NOT NULL,
-  `category` varchar(255) NOT NULL,
+  `id` int NOT NULL,
+  `category` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `date_created` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -55,14 +56,14 @@ CREATE TABLE `category` (
 --
 
 CREATE TABLE `livestock` (
-  `id` int(11) NOT NULL,
-  `category` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `quantity` int(100) NOT NULL,
-  `price` int(100) NOT NULL,
+  `id` int NOT NULL,
+  `category` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `quantity` int NOT NULL,
+  `price` int NOT NULL,
   `is_vaccinated` tinyint(1) NOT NULL,
-  `condition_notes` longtext NOT NULL,
-  `health_score` int(11) NOT NULL COMMENT 'e.g., 1–100 scale if 100 quantity',
+  `condition_notes` longtext COLLATE utf8mb4_general_ci NOT NULL,
+  `health_score` int NOT NULL COMMENT 'e.g., 1–100 scale if 100 quantity',
   `date_created` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -71,7 +72,7 @@ CREATE TABLE `livestock` (
 --
 
 INSERT INTO `livestock` (`id`, `category`, `name`, `quantity`, `price`, `is_vaccinated`, `condition_notes`, `health_score`, `date_created`) VALUES
-(3, 'Poultry', '45 Days Chicken', 50, 180, 0, 'Healthy Chicken', 50, '2026-04-22'),
+(3, 'Poultry', '45 Days Chicken', 50, 200, 0, 'Healthy Chicken', 50, '2026-04-22'),
 (4, 'Cattle', '44', 44, 44, 0, '44', 445, '2026-04-22'),
 (5, 'Cattle', '11', 1, 11, 0, '11', 11, '2026-04-22'),
 (10, 'Cattle', '3', 31, 3, 0, '3', 3, '2026-04-22'),
@@ -90,12 +91,17 @@ INSERT INTO `livestock` (`id`, `category`, `name`, `quantity`, `price`, `is_vacc
 --
 
 CREATE TABLE `orders` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `order_status` enum('pending','paid','shipped','delivered','cancelled') DEFAULT 'pending',
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `order_status` enum('pending','paid','shipped','delivered','cancelled') COLLATE utf8mb4_general_ci DEFAULT 'pending',
   `total_amount` decimal(10,2) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+  `mode_of_payment` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `notes` longtext COLLATE utf8mb4_general_ci NOT NULL,
+  `proof_of_payment` longtext COLLATE utf8mb4_general_ci,
+  `prefered_delivery_date` date NOT NULL,
+  `order_ids` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -105,10 +111,10 @@ CREATE TABLE `orders` (
 --
 
 CREATE TABLE `order_items` (
-  `id` int(11) NOT NULL,
-  `order_id` int(11) NOT NULL,
-  `product_id` int(11) NOT NULL,
-  `quantity` int(11) NOT NULL,
+  `id` int NOT NULL,
+  `order_id` int NOT NULL,
+  `product_id` int NOT NULL,
+  `quantity` int NOT NULL,
   `price` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -119,16 +125,16 @@ CREATE TABLE `order_items` (
 --
 
 CREATE TABLE `user_profile` (
-  `id` int(11) NOT NULL,
-  `first_name` varchar(255) NOT NULL,
-  `middle_name` varchar(255) NOT NULL,
-  `last_name` varchar(255) NOT NULL,
-  `contact_number` varchar(255) NOT NULL,
-  `email_address` varchar(255) NOT NULL,
-  `address` longtext NOT NULL,
-  `user_role` varchar(255) NOT NULL COMMENT 'admin, staff, user',
+  `id` int NOT NULL,
+  `first_name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `middle_name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `last_name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `contact_number` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `email_address` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `address` longtext COLLATE utf8mb4_general_ci NOT NULL,
+  `user_role` varchar(255) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'admin, staff, user',
   `date_created` date NOT NULL,
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -136,9 +142,9 @@ CREATE TABLE `user_profile` (
 --
 
 INSERT INTO `user_profile` (`id`, `first_name`, `middle_name`, `last_name`, `contact_number`, `email_address`, `address`, `user_role`, `date_created`, `password`) VALUES
-(1, 'John', 'D', 'Doe', '09123242425', 'johndoe@gmail.com', 'Purok 1', 'Administrator', '2026-04-20', ''),
-(2, 'Jimmy1', 'Pototoy', 'Bondoc', '094234513', 'aa@gmail.com', 'Purok 3', 'Staff', '2026-04-21', '$2y$10$6RB9N.ZW/frjTbaoxoAMGeWCPIzsSSBCY.47MFm.vCLVDJ9KVZ6l6'),
-(12, 'Ramil', 'Bobo', 'Madriaga', '2423', 'aa@gmail.com', 'dsf gdfgd gdsgs', 'Staff', '2026-04-22', '$2y$10$.Ol43CQOkeTaPhcb/BZ7l.pwRsvHdovHCawTg7bY22SaE/wjOU./.');
+(14, 'naruto', 'namikaze', 'uzumaki', '09307980536', 'uzumaki@gmail.com', 'Malvar, Batangas, Philippines', 'user', '2026-04-24', '$2y$10$dD9vMLk86KOIwLUfOW2JU.mYtY7UUVyavBOmBID.vMR/SxDFn1Eca'),
+(15, 'john', 'john', 'john', '09307980536', 'john@gmail.com', 'Malvar, Batangas, Philippines', 'user', '2026-04-26', '12345678'),
+(16, 'jeje update', 'jeje', 'jeje', '09307980536', 'jeje@gmail.com', 'Malvar, Batangas, Philippines', 'user', '2026-04-26', 'lance123');
 
 --
 -- Indexes for dumped tables
@@ -193,37 +199,37 @@ ALTER TABLE `user_profile`
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `livestock`
 --
 ALTER TABLE `livestock`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `order_items`
 --
 ALTER TABLE `order_items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user_profile`
 --
 ALTER TABLE `user_profile`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- Constraints for dumped tables

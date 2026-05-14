8795e5ebb1fbea0caac761cf4dbb2c1ecc9b4658
unknown <ladrera21@gmail.com>
2026-05-14 21:05:59 +0800

    add tables

---
+++ database/payroll.sql
@@ -0,0 +1,79 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 14, 2026 at 03:05 PM
+-- Server version: 8.0.34
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `saint_vincent_farm`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payroll`
+--
+
+CREATE TABLE `payroll` (
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `period_start` date NOT NULL,
+  `period_end` date NOT NULL,
+  `daily_rate` float NOT NULL,
+  `created_at` date NOT NULL,
+  `ot_pay` float NOT NULL,
+  `sss` float NOT NULL,
+  `pagibig` float NOT NULL,
+  `philhealth` float NOT NULL,
+  `late_deduction` float NOT NULL,
+  `other_deduction` float NOT NULL,
+  `net_pay` float NOT NULL,
+  `status` float NOT NULL,
+  `total_deduction` float NOT NULL,
+  `basic_pay` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `payroll`
+--
+
+INSERT INTO `payroll` (`id`, `user_id`, `period_start`, `period_end`, `daily_rate`, `created_at`, `ot_pay`, `sss`, `pagibig`, `philhealth`, `late_deduction`, `other_deduction`, `net_pay`, `status`, `total_deduction`, `basic_pay`) VALUES
+(1, 15, '2026-05-14', '2026-05-18', 100, '2026-05-14', 400, 50, 50, 50, 30, 0, 2320, 0, 180, 2100);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `payroll`
+--
+ALTER TABLE `payroll`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `payroll`
+--
+ALTER TABLE `payroll`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
